e52a1cccdfc8783a797f9d4943290f05ea7e45e2
Andrew Cassin <acassin999@gmail.com>
2021-06-13 11:01:11 +1000

    Start on ABS indicator API data ingest. Not working yet

--- src/viewer/abs/models.py
+++ src/viewer/abs/models.py
@@ -1,11 +1,18 @@
 import django.db.models as model
 from djongo.models import ObjectIdField, DjongoManager
+from django.conf import settings
+import json
+from typing import Iterable
+import pandas as pd
+import pandasdmx as sdmx
+
 
 class ABSDataCache(model.Model):
-    """ 
+    """
     Similar to, but separate from, app.MarketQuoteCache, this keeps track of pandas dataframes (parquet format) which have
     been downloaded, cleaned and ingested into MongoDB
     """
+
     size_in_bytes = model.IntegerField()
     status = model.TextField()
     tag = model.TextField()
@@ -25,6 +32,7 @@ class ABSDataCache(model.Model):
     class Meta:
         db_table = "abs_data_cache"
 
+
 class ABSInvertedIndex(model.Model):
     id = ObjectIdField(primary_key=True, db_column="_id")
     min_date = model.DateTimeField()
@@ -34,9 +42,57 @@ class ABSInvertedIndex(model.Model):
     tag = model.TextField()
     dataset = model.TextField()
     name = model.TextField()
-    scope = model.TextField() # always 'abs' for now
+    scope = model.TextField()  # always 'abs' for now
 
     objects = DjongoManager()
 
     class Meta:
         db_table = "abs_inverted_index"
+
+
+class ABSDataflow(model.Model):
+    abs_id = model.TextField()
+    name = model.TextField()
+    objects = DjongoManager()
+
+
+def data(dataflow_id: str, abs_api_key: str = None) -> pd.DataFrame:
+    assert len(dataflow_id) > 0
+    if abs_api_key is None:
+        abs_api_key = settings.ABS_API_KEY
+    headers = {
+        # "Accept": "application/json", # sdmx api doesnt support application/json, but luckily both ABS site and SDMX api support text/json
+        "Accept": "text/json",
+        "x-api-key": abs_api_key,
+    }
+    resp = sdmx.api.read_url(
+        f"https://indicator.data.abs.gov.au/data/{dataflow_id}", headers=headers
+    )
+    df = sdmx.to_pandas(resp, rtype="compat")
+    print(df)
+    assert isinstance(df, pd.DataFrame)
+
+    return df
+
+
+def dataflows(update: bool = False, abs_api_key: str = None) -> Iterable[ABSDataflow]:
+    if update or ABSDataflow.objects.count() < 1:
+        if abs_api_key is None:
+            abs_api_key = settings.ABS_API_KEY
+        headers = {
+            "Accept": "application/json",
+            "x-api-key": abs_api_key,
+        }
+        resp = requests.get(
+            "https://indicator.data.abs.gov.au/dataflows", headers=headers
+        )
+        data = json.loads(resp.content)
+        assert "resources" in data
+        # print(data)
+        for k, v in data["references"].items():
+            # print(v)
+            ABSDataflow.objects.update_or_create(
+                abs_id=v["id"], defaults={"abs_id": v["id"], "name": v["name"]}
+            )
+
+    return ABSDataflow.objects.all()
--- src/viewer/abs/views.py
+++ src/viewer/abs/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
-from django.http import Http404
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic.list import MultipleObjectTemplateResponseMixin
+from django.views.generic.edit import FormView
+from django.shortcuts import render
+from abs.forms import ABSDataflowForm
+from abs.models import data
+
 
 def abs_index_view(request):
-    raise Http404('Not implemented yet')
+    raise Http404("Not implemented yet")
+
+
+class ABSHeadlineView(LoginRequiredMixin, FormView):
+    form_class = ABSDataflowForm
+    template_name = "abs_dataset.html"
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        data_df = data(form.cleaned_data["dataflow"])
+        return render(self.request, template_name=self.template_name, context=context)
+
+
+abs_headlines = ABSHeadlineView.as_view()
