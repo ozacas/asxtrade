fb5348659e09fc1d2fa939c221eb9e85bfae5246
Andrew Cassin <acassin999@gmail.com>
2021-06-19 19:50:01 +1000

    Fixes for all plots - one dataset appears to be unavailable

--- src/viewer/abs/views.py
+++ src/viewer/abs/views.py
@@ -19,47 +19,46 @@ def abs_index_view(request):
 class ABSHeadlineView(LoginRequiredMixin, FormView):
     form_class = ABSDataflowForm
     template_name = "abs_dataset.html"
+    fixed_datapoints = None  # set used to perform de-dupe
 
     def plot_abs_dataframe(self, df: pd.DataFrame) -> p9.ggplot:
-        title = ""
         facets = []
         n_per_facet = {}
         for col in df.columns:
             try:
                 n_values = df[col].nunique()
+                if n_values == 1 and col not in [
+                    "TIME_PERIOD",
+                    "value",
+                    "Measure",
+                    "OBS_COMMENT",
+                ]:
+                    self.fixed_datapoints.add(f"{col}={df.at[0, col]}")
+                elif n_values > 1 and col not in [
+                    "value",
+                    "TIME_PERIOD",
+                    "OBS_COMMENT",
+                ]:
+                    facets.append(col)
+                    n_per_facet[col] = n_values
             except:
                 print(f"Ignoring unusable column: {col}")
                 continue
-            if n_values == 1 and col not in [
-                "TIME_PERIOD",
-                "value",
-                "Measure",
-                "OBS_COMMENT",
-            ]:
-                title += f"{col}={df.at[0, col]}"
-            elif n_values > 1 and col not in ["value", "TIME_PERIOD", "OBS_COMMENT"]:
-                facets.append(col)
-                n_per_facet[col] = n_values
+
         extra_args = {}
 
         if len(facets) > 2:
             # can only use two variables as plotting facets, third value will be used as a group on each plot
             # any more facets is not supported at this stage
             sorted_facets = sorted(n_per_facet.keys(), key=lambda k: n_per_facet[k])
-            print(n_per_facet)
-            print(sorted_facets)
+            # print(n_per_facet)
+            # print(sorted_facets)
             facets = sorted_facets[-2:]
             extra_args.update({"group": sorted_facets[0], "color": facets[0]})
             print(f"Using {facets} as facets, {extra_args} as series")
-
-        plot = p9.ggplot(
-            df, p9.aes(x="TIME_PERIOD", y="value", **extra_args)
-        ) + p9.geom_point(size=3)
-
-        if len(facets) > 0 and len(facets) <= 2:
-            plot += p9.facet_wrap(
-                "~" + " + ".join(facets[:2]), ncol=len(facets), scales="free_y"
-            )
+        else:
+            if len(facets) > 0:
+                extra_args.update({"color": facets[0]})
 
         # compute figure size to give enough room for each plot
         mult = 1
@@ -68,23 +67,48 @@ class ABSHeadlineView(LoginRequiredMixin, FormView):
         mult /= len(facets)
         nrow = int(mult + 1)
 
+        # facet column names must not have spaces in them as this is not permitted by plotnine facet formulas
+        if len(facets) > 0:
+            new_facets = []
+            for f in facets:
+                if " " in f:
+                    new_name = f.replace(" ", "_")
+                    df = df.rename(columns={f: new_name})
+                    new_facets.append(new_name)
+                else:
+                    new_facets.append(f)
+            facets = new_facets
+            print(f"Renamed facet columns due to whitespace: {facets}")
+
+        print(df)
+
+        plot = p9.ggplot(
+            df, p9.aes(x="TIME_PERIOD", y="value", **extra_args)
+        ) + p9.geom_point(size=3)
+
+        if len(facets) > 0 and len(facets) <= 2:
+            facet_str = "~" + " + ".join(facets[:2])
+            print(f"Using facet formula: {facet_str}")
+            plot += p9.facet_wrap(facet_str, ncol=len(facets), scales="free_y")
+
         plot_theme = {
             "figure_size": (12, int(nrow * 1.5)),
-            "asxtrade_want_fill_d": True,
         }
         if (
             len(facets) == 2
         ):  # two columns of plots? if so, make sure  space for axis labels
             plot_theme.update({"subplots_adjust": {"wspace": 0.2}})
-        return user_theme(plot, title=title, **plot_theme)
+        return user_theme(plot, **plot_theme)
 
     def form_valid(self, form):
         context = self.get_context_data()
         key = form.cleaned_data["dataflow"]
         data_df = data(key)
+        self.fixed_datapoints = set()  # required to perform de-dupe
         context.update(
             {
                 "plot_title": key,
+                "plot_fixed_datapoints": self.fixed_datapoints,
                 "plot_uri": cache_plot(
                     f"{key}-abs-plot",
                     lambda: self.plot_abs_dataframe(data_df),
